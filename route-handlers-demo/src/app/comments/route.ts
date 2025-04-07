ff0c45e74e142a6e0041dd12ba4017a96772dde2
CodingS31 <codingsprint31@gmail.com>
2025-04-07 17:26:18 +0530

    added get and post request

---
+++ route-handlers-demo/src/app/comments/route.ts
@@ -0,0 +1,22 @@
+// app/comments/route.ts
+import { comments } from "./data";
+
+export async function GET() {
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const newComment = {
+    id: comments.length + 1,
+    text: body.text,
+  };
+
+  comments.push(newComment);
+
+  return new Response(JSON.stringify(newComment), {
+    status: 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
