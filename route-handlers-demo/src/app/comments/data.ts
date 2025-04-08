3af19dcb355ab5914ab26c9360812b21d2028972
CodingS31 <codingsprint31@gmail.com>
2025-04-08 18:25:58 +0530

    added middleware

--- route-handlers-demo/src/app/comments/data.ts
+++ route-handlers-demo/src/app/comments/data.ts
@@ -1,5 +1,8 @@
 // app/comments/data.ts
-export const comments = [
+export const comments: {
+  id: number;
+  text: string;
+}[] = [
   { id: 1, text: "First comment" },
   { id: 2, text: "Second comment" },
   { id: 3, text: "Third comment" },
--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,6 @@
 //app/api/profile/route.ts
 
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -8,9 +9,25 @@ export async function GET(request: NextRequest) {
 
   console.log("Authorization:", authToken);
 
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log("cookies list ", theme);
+
+  const cookieStore = await cookies();
+
+  // Set a cookie
+  cookieStore.set("resultsPerPage", "20");
+
+  // Get a cookie
+  const result = cookieStore.get("resultsPerPage");
+  console.log("resultsPerPage", result); // { name: 'resultsPerPage', value: '20', path: "/" }
+
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
